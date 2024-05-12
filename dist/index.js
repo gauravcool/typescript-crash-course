99331d3d12786fa7e5f7a44b09c3385db5f61023
Gaurav Kumar <gauravkumar7aug92@gmail.com>
2024-05-12 22:10:32 +0200

    initial setup

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let id = 5;
+let company = 'Volkswagen GS';
+let isPublished = true;
+let x = 'Hello';
+x = 5;
+console.log('Id:', id);
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+let id: number = 5;
+let company: string = 'Volkswagen GS';
+let isPublished: boolean = true;
+let x: any = 'Hello';
+
+x = 5;
+
+console.log('Id:', id);
