94a26019a89e5741b9e1f7b026ce2d796e5d0f94
Gaurav Kumar <gauravkumar7aug92@gmail.com>
2024-05-12 22:59:41 +0200

    tuple, union, enum, objects

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,35 @@
 "use strict";
+// Basic Types
 let id = 5;
 let company = 'Volkswagen GS';
 let isPublished = true;
 let x = 'Hello';
-x = 5;
-console.log('Id:', id);
+let ids = [1, 4, 2, 6, 9];
+let arr = [1, 4, true, 'ada'];
+// Tuple
+let person = [1, 'Jack', true];
+//Tuple Array
+let employee;
+employee = [
+    [1, 'John'],
+    [2, 'Jane'],
+    [3, 'Jill'],
+    [4, 'Gary']
+];
+//Union
+let pId;
+pId = 789;
+pId = 'SAUL';
+//Enum
+var Direction1;
+(function (Direction1) {
+    Direction1["Up"] = "Up";
+    Direction1["Down"] = "D";
+    Direction1["Left"] = "l";
+    Direction1["Right"] = "r";
+})(Direction1 || (Direction1 = {}));
+console.log(Direction1.Down);
+const user = {
+    id: 1,
+    name: 'Brett'
+};
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,44 @@
-let id: number = 5;
-let company: string = 'Volkswagen GS';
-let isPublished: boolean = true;
-let x: any = 'Hello';
+// Basic Types
+let id: number = 5
+let company: string = 'Volkswagen GS'
+let isPublished: boolean = true
+let x: any = 'Hello'
 
-x = 5;
+let ids: number[] = [1, 4, 2, 6, 9]
+let arr: any[] = [1, 4, true, 'ada']
 
-console.log('Id:', id);
+// Tuple
+let person: [number, string, boolean] = [1, 'Jack', true]
+//Tuple Array
+let employee: [number, string][]
+employee = [
+    [1, 'John'],
+    [2, 'Jane'],
+    [3, 'Jill'],
+    [4, 'Gary']
+]
+
+//Union
+let pId: number | string
+pId = 789
+pId = 'SAUL'
+
+//Enum
+enum Direction1 {
+    Up='Up',
+    Down='D',
+    Left='l',
+    Right='r'
+}
+console.log(Direction1.Down)
+
+// Objects
+type User = {
+    id: number,
+    name: string
+}
+
+const user: User = {
+    id: 1,
+    name: 'Brett'
+}
